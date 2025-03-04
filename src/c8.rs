1f8d849721c5e3e91e6a3acd60451fe945208bb3
LuigiDotSRC <luigi.terashima@gmail.com>
2025-03-03 22:28:23 -0500

    implement fontset, add poc emulate_cycle and read_program

--- src/c8.rs
+++ src/c8.rs
@@ -1,35 +1,64 @@
+use crate::font;
+
 pub struct C8 {
     memory: [u8; 4096],
     v_regs: [u8; 16], // V0 to VF
-    i_reg: u8,
-    pc: u8, 
-    opcode: [u8; 2],
+    i_reg: u16,
+    pc: u16, 
+    opcode: u16,
 
-    gfx: [[bool; 64]; 32],
+    gfx: [[u8; 64]; 32],
 
     delay_timer: u8,
     sound_timer: u8,
 
-    stack: [u8; 16],// 16 levels of nesting
-    sp: u8,
+    stack: [u16; 16],// 16 levels of nesting
+    sp: u16,
     
-    key: [bool; 16], // hex keyboard
+    key: [u8; 16], // hex keyboard
 }
 
 impl C8 {
     pub fn new() -> Self {
-        Self {
+        let mut instance = Self {
             memory: [0x00; 4096],
             v_regs: [0x00; 16],
-            i_reg: 0x00,
-            pc: 0x00,
-            opcode: [0x00; 2],
-            gfx: [[false; 64]; 32],
+            i_reg: 0x0000,
+            pc: 0x200, // programs expected to start at 0x200
+            opcode: 0x0000,
+            gfx: [[0x00; 64]; 32],
             delay_timer: 0x00,
             sound_timer: 0x00,
-            stack: [0x00; 16],
-            sp: 0x00,
-            key: [false; 16]
+            stack: [0x0000; 16],
+            sp: 0x0000,
+            key: [0x00; 16]
+        };
+
+        // load fontset
+        for i in 0..79 {
+            instance.memory[i] = font::CHIP8_FONTSET[i];
         }
+
+        instance
+    }
+
+    pub fn read_program() {
+        // read a binary file
+
+        // load it into memory starting at 0x200
+    }
+
+    pub fn emulate_cycle(&mut self) {
+        // test
+        self.memory[0] = 0xA2;
+        self.memory[1] = 0xF0;
+        
+        self.opcode = (self.memory[0] as u16) << 8 | (self.memory[1] as u16);
+
+        self.i_reg = self.opcode & 0x0FFF;
+
+        self.pc += 2;
+        println!("{:X}", self.opcode);
+        println!("{:X}", self.i_reg);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod c8;
+mod font;
 
 use c8::C8;
 
+const OPCODES_PER_SECOND: usize = 500;
+
 fn main() {
-    let chip_8: C8 = C8::new();
-    println!("done compiling");
+    // setup graphics
+    // setup input
+
+    let mut chip_8: C8 = C8::new();
+    
+    // load game
+
+    // emulation loop
+
+        // emulate cycle
+        chip_8.emulate_cycle()
+        // drawGraphics if draw fag is set
+
+        // store key press states
 }
